07be3c8717473766df1ebb52e755ac536bbfd6dd
frnsfrmnsyh10@gmail.com <frnsfrmnsyh10@gmail.com>
2023-09-16 14:12:42 +0700

    jbdcjdbchdbhdb

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,16 +14,16 @@ window.onscroll = function(){
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-document.onclick = function(e){
+// document.onclick = function(e){
 
-    if(e.target.id !== 'hamburger' && e.target.id !== 'nav-menu')
+//     if(e.target.id !== 'nav-menu' && e.target.id !== 'hamburger')
     
-    {
-    hamburger.classList.remove('hamburger-active');
-    navMenu.classList.remove('hidden');
-    navMenu.classList.toggle('hidden')
-    }
-    }
+//     {
+//     hamburger.classList.remove('hamburger-active');
+//     navMenu.classList.remove('hidden');
+//     navMenu.classList.toggle('hidden')
+//     }
+//     }
 
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
@@ -31,3 +31,8 @@ hamburger.addEventListener('click', function () {
 
 });
 
+if (iosVersion >= 7) {
+    $(document).scroll(function() {
+        $('#background').css('background-position', '0px ' + $(document).scrollTop() + 'px');
+    });
+}
