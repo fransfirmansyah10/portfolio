9a372e09934bcea89b82974111bbf4478a8ac41c
frnsfrmnsyh10@gmail.com <frnsfrmnsyh10@gmail.com>
2024-01-22 11:23:40 +0700

    W

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,34 @@ module.exports = {
       screens: {
       '2xl' : '1320px',
       },
+
+      keyframes:{
+        'trans-down1':{
+          '0% , 100%':{ transform:'translateY(10px)'},
+          '50%':{transform:'translateY(0)'}
+        },
+        'trans-down2':{
+          '0% , 100%':{ transform:'translateY(0px)'},
+          '50%':{transform:'translateY(20px)'}
+        },
+        'trans-down3':{
+          '0% , 100%':{ transform:'translateY(30px)'},
+          '50%':{transform:'translateY(0px)'}
+        },
+
+        'trans-down4':{
+          '0% , 100%':{ transform:'translateY(20px)'},
+          '30%':{transform:'translateY(0px)'}
+        }
+
+      },
+      animation:{
+        'trans-down1':'trans-down1 2s ease-in-out infinite',
+        'trans-down2':'trans-down2 4s ease-in-out infinite',
+        'trans-down3':'trans-down3 10s ease-in-out infinite',
+        'trans-down4':'trans-down4 10s ease-in-out infinite'
+      }
+
     },
   },
   plugins: [],
